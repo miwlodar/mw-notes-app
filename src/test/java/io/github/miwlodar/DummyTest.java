a09cb9c7af32eed3b8d49cebf4be1eafc7b851d9
Kamil Pietruszka <kamil.pietruszka@superside.com>
2022-08-24 19:02:46 +0200

    build: setup testing environment
     - Add surefire-plugin: used during the test phase of the build lifecycle to execute the unit tests of an application
     - Add junit-api: JUnit Platform serves as a foundation for launching testing frameworks on the JVM
     - Add DummyTest: To prove the tests are being run properly with the maven build tool.
     - Add passing test that is being run and pass
     - Add failing test with @Disabled annotation - this test is ignored

---
+++ src/test/java/io/github/miwlodar/DummyTest.java
@@ -0,0 +1,20 @@
+package io.github.miwlodar;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class DummyTest {
+
+    @Test
+    void should_always_pass() {
+        assertThat(true).isTrue();
+    }
+
+    @Disabled("Ignored test case, never comment out tests.")
+    void should_always_fail() {
+        fail("I will always fail");
+    }
+}
